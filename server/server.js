7e69664ff699018162a2e616c38cf5374347c41c
Ransom <153246367+NiccBot@users.noreply.github.com>
2025-04-16 09:40:47 -0400

    Fleshing out server a little bit, now has basic functionality

--- server/server.js
+++ server/server.js
@@ -1,13 +1,17 @@
 const https = require('https');
 const fs = require('fs');
+const express = require('express');
+const app = express();
 
 var connections = [];
 var num_games = 0;
 app.use(express.json());
 class Game{
     
+
+
     constructor(){
-        this.gameID = num_games
+        this.gameID = num_games++;
         
     }
 
@@ -18,17 +22,26 @@ class Game{
 
 // Client opens a lobby, returns a game code for the lobby
 app.post('/startgame', (req, res) => {
-    
+    res.sendFile('../client/gamepage.html', {root: __dirname});
+    console.log("Host connected, waiting for second player");
 })
 
 // Guest client joins lobby with game code
 app.post('/login', (req, res) => {
+    res.sendFile('../client/gamepage.html', {root: __dirname});
+    console.log("Guest connected, starting game...");
 
+})
+
+// Guest client connects to the website
+app.get('/', (req, res) => {
+    console.log(__dirname);
+    res.sendFile('../client/login.html', {root: __dirname});
 })
 
 // Client posts initial position and rotation of their ships
 app.post('/setup', (req, res) => {
-
+    
 
 })
 
@@ -36,4 +49,8 @@ app.post('/setup', (req, res) => {
 app.post('/shoot', (req, res) => {
 
 
+})
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
 })
