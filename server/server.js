2af5387b31e864b0dbd0f6633deb01f6bad4504b
Ransom <153246367+NiccBot@users.noreply.github.com>
2025-04-23 09:29:48 -0400

    Server test

--- server/server.js
+++ server/server.js
@@ -2,16 +2,20 @@ const https = require('https');
 const fs = require('fs');
 const express = require('express');
 const app = express();
+const path = require('path');
+const { randomInt } = require('crypto');
 
 var connections = [];
-var num_games = 0;
 app.use(express.json());
 class Game{
     
 
 
     constructor(){
-        this.gameID = num_games++;
+        this.gameID = randomInt(100,999).toString();
+        this.hostTurn = true;
+        this.host = null;
+        this.guest = null;
         
     }
 
@@ -24,19 +28,33 @@ class Game{
 app.post('/startgame', (req, res) => {
     res.sendFile('../client/gamepage.html', {root: __dirname});
     console.log("Host connected, waiting for second player");
+    let game = new Game();
+    game.host = req.body.hostName;
+    connections.push(game);
+    res.status(200).json({ gameID: game.gameID });
 })
 
 // Guest client joins lobby with game code
 app.post('/login', (req, res) => {
-    res.sendFile('../client/gamepage.html', {root: __dirname});
+    let filePath = path.join(__dirname, '../client/gamepage.html')
+    res.sendFile(filePath);
     console.log("Guest connected, starting game...");
 
+    const { gameID } = req.body;
+    const game = connections.find(g => g.gameID === gameID);
+    if (game) {
+        game.guest = req.body.guestName;
+        res.status(200).json({ message: "Guest joined the game", gameID: game.gameID });
+    } else {
+        res.status(404).json({ message: "Game not found" });
+    }
 })
 
 // Guest client connects to the website
 app.get('/', (req, res) => {
-    console.log(__dirname);
-    res.sendFile('../client/login.html', {root: __dirname});
+    let filePath = path.join(__dirname, '../client/login.html');
+    res.sendFile(filePath);
+    console.log("User connected to server");
 })
 
 // Client posts initial position and rotation of their ships
@@ -45,7 +63,7 @@ app.post('/setup', (req, res) => {
 
 })
 
-// A player takes a shotd
+// A player takes a shot
 app.post('/shoot', (req, res) => {
 
 
